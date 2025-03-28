7c656848202c04b035a39400235bec731c7a347d
josue081 <josue.moraes23@etepd.com>
2025-03-27 21:02:44 -0300

    Update EX 1(C).py

--- josu.AT1/EX 1(C).py
+++ josu.AT1/EX 1(C).py
@@ -1,15 +1,15 @@
-nota1 = float(input("Nota 1:"))
-peso1 = 1
+nota1 = float(input("Nota 2:"))
+peso1 = 2
 
 
-nota2 = float(input("Nota 2:"))
-peso2 = 2
+nota2 = float(input("Nota 3:"))
+peso2 = 3
 
-nota3 = float(input("Nota 3:"))
-peso3 = 2
+nota3 = float(input("Nota 4:"))
+peso3 = 4
 
 
-media = (nota1*peso1 + nota2*peso2 + nota3*peso3) / (peso1+peso2+peso3)
+media = (nota2*peso2 + nota3*peso3 + nota4*peso4) / (peso1+peso3+peso4)
 
 
 
