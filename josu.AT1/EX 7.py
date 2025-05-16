187883db0e56d068ddc05dca08d8e54377da3eba
Josue_Moraes <josue.moraes23@etepd.com>
2025-05-16 14:18:47 -0300

    Update EX 7.py

--- josu.AT1/EX 7.py
+++ josu.AT1/EX 7.py
@@ -1,18 +1,9 @@
-numero = int(input("Digite um número ímpar: "))
+n = int(input("Número ímpar: "))
 
-
-if numero % 2 == 0:
-    print("O número digitado não é ímpar. Tente novamente.")
+if n % 2 == 0:
+    print("Não é ímpar.")
 else:
-    numero_anterior = numero - 2
-    numero_proximo = numero + 2
-
-
-    quadrado_anterior = numero_anterior ** 2
-    quadrado_proximo = numero_proximo ** 2
-
-
-    diferenca = quadrado_anterior - quadrado_proximo
-
-
-    print(f"A diferença entre os quadrados de {numero_anterior} e {numero_proximo} é: {diferenca}")
+    antes = n - 2
+    depois = n + 2
+    dif = (antes ** 2) - (depois ** 2)
+    print("Diferença:", dif)
