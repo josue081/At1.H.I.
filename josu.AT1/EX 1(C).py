52d73e5535680aba01230e2e67c60c28fb031fd1
Josue_Moraes <josue.moraes23@etepd.com>
2025-05-16 13:36:39 -0300

    Update EX 1 (A), (B), (C).

--- josu.AT1/EX 1(C).py
+++ josu.AT1/EX 1(C).py
@@ -1,15 +1,17 @@
+# Agr vai
+
 nota1 = float(input("Nota 1: "))
 nota2 = float(input("Nota 2: "))
 nota3 = float(input("Nota 3: "))
 
-# a. Média Aritmética Simples
+# a. A média aritmética simples
 media_simples = (nota1 + nota2 + nota3) / 3
 print("Média Simples:", media_simples)
 
-# b. Média Ponderada (2, 2, 3)
+# b. A média ponderada considerando os pesos 2, 2 e 3
 media_ponderada_1 = (nota1*2 + nota2*2 + nota3*3) / (2 + 2 + 3)
 print("Média Ponderada (2,2,3):", media_ponderada_1)
 
-# c. Média Ponderada (1, 2, 2)
+# c. A média ponderada considerando os pesos 1, 2 e 2 
 media_ponderada_2 = (nota1*1 + nota2*2 + nota3*2) / (1 + 2 + 2)
 print("Média Ponderada (1,2,2):", media_ponderada_2)
