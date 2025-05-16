2d83ffebbf32160b2aba12c5dab6ff41140ef5f6
Josue_Moraes <josue.moraes23@etepd.com>
2025-05-16 14:40:04 -0300

    Update EX 12.py

--- josu.AT1/EX 12.py
+++ josu.AT1/EX 12.py
@@ -1,18 +1,11 @@
-quantidade_turmas = int(input("Digite a quantidade de turmas: "))
-alunos_por_turma = []
+print("calculo da média de alunos por turma.")
 
+turmas = int(input("Qual a quantidade de turmas? "))
+alunos = int(input("Qual é o total de alunos? "))
 
-for i in range(1, quantidade_turmas + 1):
-    alunos = int(input(f"Digite a quantidade de alunos na turma {i}: "))
-    alunos_por_turma.append(alunos)
-   
-    if alunos > 40:
-        print(f"Atenção: A turma {i} tem mais de 40 alunos.")
+media = alunos / turmas
 
+print("Média de alunos por turma:", media)
 
-media_alunos = sum(alunos_por_turma) / quantidade_turmas
-
-
-print(f"A média de alunos por turma é: {media_alunos:.2f}")
-
-
+if media > 40:
+    print("Atenção: tem alguma turma com mais de 40 alunos.")
